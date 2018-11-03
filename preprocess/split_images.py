bd87780debf924c65319307d0aa4ea1466e4b308
Naoya Yamashita <conao3@gmail.com>
2018-11-03 20:22:23 +0900

    edge detection

--- preprocess/split_images.py
+++ preprocess/split_images.py
@@ -11,7 +11,7 @@ class CV2Window:
         
         cv2.namedWindow(self.name)
 
-    def imgshow(self, img, wait=True):
+    def imgshow(self, img, wait=False):
         cv2.imshow(self.name, img)
         if wait:
             cv2.waitKey(0)
@@ -24,11 +24,28 @@ def fit_size(img, h, w):
     f = min(h / size[0], w / size[1])
     return cv2.resize(img, (int(size[1] * f), int(size[0] * f)), interpolation=cv2.INTER_AREA)
 
+def get_edges(img, show=True):
+    # Edge detection by canny edge detection.
+    # IMG required to be a color numpy array image.
+    
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    edges = cv2.Canny(gray, 50, 150)
+    if show:
+        window = CV2Window('edges')
+        window.imgshow(edges)
+        
+    return edges
+
+def line(img, show=True, threshold=80, minLineLength=50, maxLineGap=5):
+    edges = get_edges(img, True)
+    lines = cv2.HoughLinesP(edges, 1, np.pi/180, threshold, 300, minLineLength, maxLineGap)
+    
 def main():
     mainWindow = CV2Window('main')
-    
-    print("loading %d files..." % len(files))
+
     files = sorted(glob.glob('../images/raw/*'))
+
+    print("loading %d files..." % len(files))
     raw_imgs = [cv2.imread(f) for f in files]
 
     print("resizing...")
@@ -39,6 +56,9 @@ def main():
     img = imgs[1]
     mainWindow.imgshow(img)
 
+    line(img)
+    line(img)
+    cv2.waitKey(0)
 
 if __name__ == '__main__':
     main()
