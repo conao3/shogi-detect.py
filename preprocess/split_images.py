5b34b17e67c91574544044e743acf044bfa474fb
Naoya Yamashita <conao3@gmail.com>
2018-11-03 19:02:50 +0900

    load imgs

--- preprocess/split_images.py
+++ preprocess/split_images.py
@@ -5,12 +5,25 @@ import numpy as np
 import itertools
 import glob
 
+def CV2Window:
+    def __init__(self, _name):
+        self.name = _name
+        
+        cv2.namedWindow(self.name)
+
+    def imgshow(img, wait=True):
+        cv2.imshow(img)
+        if wait:
+            cv2.waitKey(0)
+
 def fit_size(img, h, w):
     size = img.shape[:2]
     f = min(h / size[0], w / size[1])
     return cv2.resize(img, (int(size[1] * f), int(size[0] * f)), interpolation=cv2.INTER_AREA)
 
 def main():
+    mainWindow = CV2Window('main')
+    
     files = sorted(glob.glob('../images/raw/*'))
     print(len(files), "files")
     
@@ -18,7 +31,8 @@ def main():
     imgs = [fit_size(img, 500, 500) for img in raw_imgs]
     raw_img = raw_imgs[1]
     img = imgs[1]
-    cv2.imshow('img', img)
+    mainWindow.imgshow(img)
+
 
 if __name__ == '__main__':
     main()
