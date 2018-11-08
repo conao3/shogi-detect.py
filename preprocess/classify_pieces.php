dc44a4647b571aba5fa7bbc360126e72b4a78e5b
Naoya Yamashita <conao3@gmail.com>
2018-11-08 16:45:32 +0900

    remove margin

--- preprocess/classify_pieces.php
+++ preprocess/classify_pieces.php
@@ -1,37 +1,42 @@
 <html>
   <head>
     <meta charset="utf-8">
-    
+
+    <style type="text/css">
+      img {
+	  margin:-2pt;
+      }
+    </style>
     <script type="text/javascript">
       window.onload = function onLoad () {
-      var images = document.querySelectorAll('img');
-      images.forEach(function(image) {
-      image.addEventListener("click", function() {
-      // クリックされたらstyle属性を直接書き換える
-      image.style.opacity = '0.5';
-      }, false);
+	  var images = document.querySelectorAll('img');
+	  images.forEach(function(image) {
+	      image.addEventListener("click", function() {
+		  // クリックされたらstyle属性を直接書き換える
+		  image.style.opacity = '0.5';
+	      }, false);
 
-      
-      image.addEventListener("dblclick", function() {
-      // ダブルクリックされたらstyle属性を除去
-      image.removeAttribute("style");
-      }, false);
+	      
+	      image.addEventListener("dblclick", function() {
+		  // ダブルクリックされたらstyle属性を除去
+		  image.removeAttribute("style");
+	      }, false);
 
-      });
+	  });
       }
       
       function show () {
-      var images = document.querySelectorAll('img');
-      message = "mv ";
-      images.forEach(function(image) {
-      if (image.getAttribute("style")) {
-      message = message + "\"" + image.getAttribute("src") + "\" ";
-      }
-      });
+	  var images = document.querySelectorAll('img');
+	  message = "mv ";
+	  images.forEach(function(image) {
+	      if (image.getAttribute("style")) {
+		  message = message + "\"" + image.getAttribute("src") + "\" ";
+	      }
+	  });
 
-      message = message + "./selected/"
-      showplace = document.getElementById("selectplace");
-      showplace.innerHTML = message;
+	  message = message + "./selected/"
+	  showplace = document.getElementById("selectplace");
+	  showplace.innerHTML = message;
       }
     </script>
   </head>
@@ -49,12 +54,12 @@
 </html>
 
 <?php
-function showimg() {
-$filepaths = glob('./piecies/*.png');
-$i=0;
-foreach($filepaths as $filepath) {
-$i=$i+1;
-echo "<img width=\"33\", height=\"33\", src=", $filepath, ">";
+ function showimg() {
+ $filepaths = glob('./piecies/*.png');
+ $i=0;
+ foreach($filepaths as $filepath) {
+ $i=$i+1;
+ echo "<img width=\"33\", height=\"33\", src=", $filepath, ">";
 echo("\n");
 }
 }
